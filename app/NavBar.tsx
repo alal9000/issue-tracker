03c8fdf82673e48044da2a5f372b56d68b60f318
Aaron Lal <lucidlear@gmail.com>
2023-12-04 16:26:43 +1100

    building the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,21 +3,25 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 
 const links = [
-  { label: "dashboard", href: "/dashboard" },
-  { label: "issues", href: "/issues" }
+  { label: "Dashboard", href: "/" },
+  { label: "Issues", href: "/issues" }
 ];
 
 const NavBar = () => {
   return (
-    <nav className="flex space-x-5 h place-items-center p-5 border-b border-gray-400 border-t border-gray-400 mb-5">
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
         <AiFillBug />
       </Link>
-      <ul className="flex space-x-5">
+      <ul className="flex space-x-6">
         {links.map((link) => (
-          <li key={link.href} className="text-slate-500 hover:text-slate-800">
-            <Link href={link.href}>{link.label}</Link>
-          </li>
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
         ))}
       </ul>
     </nav>
