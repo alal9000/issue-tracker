970e45c288824cacf842502eb1250931626dcd97
Aaron Lal <lucidlear@gmail.com>
2023-12-08 19:31:11 +1100

    removing duplicate skeletons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,7 +8,7 @@ import classnames from "classnames";
 
 const links = [
   { label: "Dashboard", href: "/" },
-  { label: "Issues", href: "/issues" }
+  { label: "Issues", href: "/issues/list" }
 ];
 
 const NavBar = () => {
